64c0d4febe75c8274379b7995bd9f0b7cfd32238
fradyv <fastunadya@gmail.com>
2026-04-14 14:49:22 +0700

    Add files via upload

---
+++ LatihanClassDanObject6503/Mahasiswa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject6503
+{
+    internal class Mahasiswa
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public float Ipk { get; set; }
+
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama : {0}", Nama);
+            Console.WriteLine("NIM: {0}", Nim);
+            Console.WriteLine("IPK: {0}", Ipk);
+            Console.WriteLine("Telah melakukan registrasi");
+        }
+
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan Krs\n", Nama);
+        }
+    }
+}
